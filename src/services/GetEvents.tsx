a719b7779577f6ab06410fe6008243f8cb609723
rachel-rebecca <Rarebecc@umich.edu>
2021-09-15 21:15:11 -0400

    put .env.local in root folder

--- src/services/GetEvents.tsx
+++ src/services/GetEvents.tsx
@@ -2,6 +2,9 @@ import Event from "../models/Event";
 import axios from "axios";
 
 export default function getEvents (): Promise<Event[]> {
-    return axios.get("https://app.ticketmaster.com/discovery/v2/events.json?postalCode=48226&apikey=hGWHw80UqtonvVa5RfP9mPRnG5sdbhnh")
+    const key = process.env.REACT_APP_API_KEY
+
+
+    return axios.get("https://app.ticketmaster.com/discovery/v2/events.json?postalCode=48226&apikey=")
     .then(response => response.data._embedded.events)
 }
