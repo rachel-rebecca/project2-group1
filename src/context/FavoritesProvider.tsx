f9ea97237f5ecae69d3c457406777732ec175429
rachel-rebecca <Rarebecc@umich.edu>
2021-09-21 19:45:00 -0400

    desktop view styling for homepage

---
+++ src/context/FavoritesProvider.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode, useState } from "react";
+import Event from "../models/Event";
+import GetDetailsInterface from "../models/GetDetailsInterface"
+
+
+
+interface FavoritesProps {
+    addToFaves: (event: Event) => void;
+    remove: (url: string) => void;
+    favoritesList: Event[];
+}
+
+const defaultValues: FavoritesProps = {
+    addToFaves: () => {},
+    remove: () => {},
+    favoritesList: [] 
+}
+
+export const Favorites = React.createContext<FavoritesProps>(defaultValues)
+
+
+export default function FavoritesProvider({children}: {children: ReactNode}) {
+    const[favoritesList, setFavoritesList] = useState<Event[]>([]);
+
+    function addToFaves(event: Event): void {
+        let newFavorites = [...favoritesList];
+        newFavorites.push(event);
+        setFavoritesList(newFavorites)
+    }
+
+    function remove(url: string): void {
+        let newFavorites = [...favoritesList];
+        let foundIndex = newFavorites.findIndex(event => event.url == url);
+        newFavorites.splice(foundIndex, 1)    
+        setFavoritesList(newFavorites)
+    }
+
+    return <Favorites.Provider value={{addToFaves, remove, favoritesList}}>{children}</Favorites.Provider>
+
+}
