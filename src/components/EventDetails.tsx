5c36c06b706e96b6f81e0bc4af99f9d468adebec
korchie <korchie44@gmail.com>
2021-09-21 00:33:11 -0400

    added some EventDetails. Need to make button for buy tickets on EventDetails pg

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -8,6 +8,9 @@ import { getDetails } from "../services/GetDetails";
 import { Embedded } from "../models/Event"
 import { VenuesEntity } from "../models/Event"
 import GetDetailsInterface from "../models/GetDetailsInterface"
+import { start } from "repl";
+import { Link } from "react-router-dom";
+
 
 interface Params {
   id: any;
@@ -21,20 +24,19 @@ export default function EventDetails() {
   const [event, setEvent] = useState<GetDetailsInterface>()
   const [eventName, setEventName] = useState("");
   const [eventUrl, setEventUrl] = useState("");
+  // const [goUrl, setGoUrl] = useState(() => { eventUrl })
+  const [eventDate, setEventDate] = useState("");
   const { id, date, name, info } = useParams<Params>();
 
   useEffect(() => {
     getDetails(id).then((data) => {
       if (data && data._embedded && data._embedded.events && data._embedded.events[0]) {
 
-        console.log(data._embedded.events[0])
-        setEventName(data._embedded.events[0].name)
-        setEventUrl(data._embedded.events[0].url)
-        // setEventName(data._embedded.events[0].date)
+        console.log(data._embedded.events[0]);
+        setEventName(data._embedded.events[0].name);
+        setEventUrl(data._embedded.events[0].url);
+        setEventDate(data._embedded.events[0].dates.start.localDate);
       }
-
-
-
     })
   }, [setEvent]);
 
@@ -42,11 +44,12 @@ export default function EventDetails() {
 
   return (
     <div className="container">
-      <h1>Event Details</h1>
-      <h2>id:{id}</h2>
-      <h2>date:{date}</h2>
-      <h2>name:{eventName}</h2>
-      <h2>url:{eventUrl}</h2>
+      <h1>Event Details:</h1>
+      {/* <h2>id:{id}</h2> */}
+      <h2>{eventName}</h2>
+      <h2>Date: {eventDate}</h2>
+      {/* <Link to={eventUrl} className="btn btn-primary">Buy Tickets</Link>  <-- ##I was trying to make a button, but failing -steph##  */}
+      <a href={eventUrl}>Buy Tickets</a>
     </div>
   );
 }
