58a1145054c436af35573d6db337c7cf403a111c
korchie <korchie44@gmail.com>
2021-09-21 21:50:12 -0400

    added times to EventDetails

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -4,7 +4,7 @@ import Event from "../models/Event";
 import { useHistory } from "react-router";
 import { getEvent } from "../services/GetEvents";
 import ResultRow from "../subcomponents/ResultRow";
-import { getDetails } from "../services/GetDetails";  
+import { getDetails } from "../services/GetDetails";
 import { Embedded } from "../models/Event"
 import { VenuesEntity } from "../models/Event"
 import GetDetailsInterface from "../models/GetDetailsInterface"
@@ -21,11 +21,12 @@ export default function EventDetails() {
   const [event, setEvent] = useState<GetDetailsInterface>()
   const [eventName, setEventName] = useState("");
   const [eventUrl, setEventUrl] = useState("");
-  const[info, setInfo] = useState("");
-  const[access, setAccess] = useState("");
-  const[healthcheck, setHealthCheck] = useState("");
+  const [info, setInfo] = useState("");
+  const [access, setAccess] = useState("");
+  const [healthcheck, setHealthCheck] = useState("");
   const [eventDate, setEventDate] = useState("");
-  const { id} = useParams<Params>();
+  const [eventTime, setEventTime] = useState("");
+  const { id } = useParams<Params>();
 
   useEffect(() => {
     getDetails(id).then((data) => {
@@ -35,36 +36,38 @@ export default function EventDetails() {
         setEventName(data._embedded.events[0].name);
         setEventUrl(data._embedded.events[0].url);
         setEventDate(data._embedded.events[0].dates.start.localDate);
+        setEventTime(data._embedded.events[0].dates.start.localTime);
         setInfo(data._embedded.events[0].info);
         setAccess(data._embedded.events[0].accessibility.info);
-        if (data._embedded.events[0].ticketing?.healthCheck?.description.length > 0){
-        setHealthCheck(data._embedded.events[0].ticketing?.healthCheck?.description)
+        if (data._embedded.events[0].ticketing?.healthCheck?.description.length > 0) {
+          setHealthCheck(data._embedded.events[0].ticketing?.healthCheck?.description)
         } else {
-            setHealthCheck("None")
+          setHealthCheck("None")
         }
       }
     })
   }, [setEvent]);
 
 
-    useEffect(() => {
-        getDetails(id).then((data) => {
-            console.log(data) 
-        })  
-    }, [setEvent]);
+  useEffect(() => {
+    getDetails(id).then((data) => {
+      console.log(data)
+    })
+  }, [setEvent]);
 
   return (
     <div className="container eventDetailsDiv">
       <h1>Event Details:</h1>
       <h2>{eventName}</h2>
-      <h2>Date: {eventDate}</h2>
+      <h4>Date: {eventDate}</h4>
+      <h4>Time: {eventTime}</h4>
       <details>
-          <summary> More Info</summary>
-            <p><strong>Note:</strong> {info} </p>
-            <p><strong>Accessibility:</strong> {access}</p>
-            <p><strong>Healthcheck:</strong> {healthcheck}</p>
+        <summary> More Info</summary>
+        <p><strong>Note:</strong> {info} </p>
+        <p><strong>Accessibility:</strong> {access}</p>
+        <p><strong>Healthcheck:</strong> {healthcheck}</p>
       </details>
-     <button><a href={eventUrl} className="buyTickets">Buy Tickets</a></button>
+      <button><a href={eventUrl} className="buyTickets">Buy Tickets</a></button>
     </div>
   );
 }
