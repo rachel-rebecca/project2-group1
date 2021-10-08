b5b2cfb3905729f488ecc48e957c731523b53932
rachel-rebecca <Rarebecc@umich.edu>
2021-10-08 16:49:00 -0400

    toml and index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from "react-router-dom"
 import cors from "cors"
+import {handler} from "../netlify/handler"
 
 ReactDOM.render(
   <React.StrictMode>
